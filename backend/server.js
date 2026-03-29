95b7c4e66669f8ff5c4e2a098ccf53cd9b342b72
Mohammad-Islam2004 <ik7877167630@gmail.com>
2026-03-29 17:22:38 +0530

    added base route

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,9 @@ app.use(express.json())
 
 connectDB()
 
+app.get("/", (req, res) => {
+  res.json({ message: "Expensewise API is live and running!" });
+});
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/income", incomeRoutes)
 app.use("/api/v1/expense", expenseRoutes)

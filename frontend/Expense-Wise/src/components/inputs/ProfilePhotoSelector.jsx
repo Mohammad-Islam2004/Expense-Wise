3547e3b726cfda9224c1cc76e1fe7b8ef8c9c76d
Mohammad-Islam2004 <ik7877167630@gmail.com>
2026-04-03 07:20:22 +0530

    SignUp UI  update

--- frontend/Expense-Wise/src/components/inputs/ProfilePhotoSelector.jsx
+++ frontend/Expense-Wise/src/components/inputs/ProfilePhotoSelector.jsx
@@ -35,7 +35,7 @@ const ProfilePhotoSelector = ({image, setImage}) => {
         className="hidden"
       />
       {!image ? (
-        <div className="w-20 h-20 flex items-center justify-center bg-purple-100 rounded-full relative">
+        <div className="w-20 h-20 flex items-center justify-center bg-gray-300 rounded-full relative">
           <LuUser className="text-4xl text-primary"/>
           <button className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full absolute -bottom-1 -right-1 cursor-pointer" type="button" onClick={onChooseFile}> <LuUpload/> </button>
         </div>
